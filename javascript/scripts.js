3e105641b04ec08391f4b3f9790b9417321af2af
RodATS <77297145+RodATS@users.noreply.github.com>
2021-04-17 23:59:17 -0500

    Update scripts.js

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,1 +1,16 @@
 //Place your javascript code related with HTML and CSS here
+function getValue(field){
+  var valor = document.getElementById(field).value;
+  return valor;
+}
+function displayResult(field,value) {
+  var result = value;
+  document.getElementById(field).innerHTML = result;
+  return false;
+}
+
+var btn = document.querySelector('.burgerbutton');
+btn.addEventListener('click', function () {
+  document.getElementById('sidebar').classList.toggle('active');
+  document.querySelector('body').classList.toggle('displaced');
+});
